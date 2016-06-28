4dfd6e03966320ae610303464e7fb5d8ffa9e1e6
Matthew Phillips <matthew@matthewphillips.info>
2016-06-28 12:58:21 -0400

    3.0.0-pre.3

---
+++ dist/cjs/steal-stache.js
@@ -0,0 +1,28 @@
+/*steal-stache@3.0.0-pre.2#steal-stache*/
+'format cjs';
+var getIntermediateAndImports = require('can-stache/src/intermediate_and_imports');
+var addBundles = require('./add-bundles.js');
+function template(imports, intermediate) {
+    imports = JSON.stringify(imports);
+    intermediate = JSON.stringify(intermediate);
+    return 'define(' + imports + ',function(module, stache, mustacheCore){\n' + '\tvar renderer = stache(' + intermediate + ');\n' + '\treturn function(scope, options, nodeList){\n' + '\t\tvar moduleOptions = { module: module };\n' + '\t\tif(!(options instanceof mustacheCore.Options)) {\n' + '\t\t\toptions = new mustacheCore.Options(options || {});\n' + '\t\t}\n' + '\t\treturn renderer(scope, options.add(moduleOptions), nodeList);\n' + '\t};\n' + '});';
+}
+function translate(load) {
+    var intermediateAndImports = getIntermediateAndImports(load.source);
+    var commonDependencies = Promise.all([
+        this.normalize('can-view-import', module.id),
+        this.normalize('can-stache-bindings', module.id)
+    ]);
+    return Promise.all([
+        addBundles(intermediateAndImports.dynamicImports, load.name),
+        commonDependencies
+    ]).then(function (results) {
+        var imports = results[1];
+        intermediateAndImports.imports.unshift.apply(intermediateAndImports.imports, imports);
+        intermediateAndImports.imports.unshift('can-stache/src/mustache_core');
+        intermediateAndImports.imports.unshift('can-stache');
+        intermediateAndImports.imports.unshift('module');
+        return template(intermediateAndImports.imports, intermediateAndImports.intermediate);
+    });
+}
+module.exports = { translate: translate };
