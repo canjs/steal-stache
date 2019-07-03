7827f4467a2b8996e158ca2ff2dbe017668e09c8
Nils Lundquist <nils@bitovi.com>
2019-07-03 17:13:15 -0600

    pass `can-import`ed modules to stache in helpers scope so they can be available synchronously

--- steal-stache.js
+++ steal-stache.js
@@ -5,22 +5,31 @@ var loader = require("@loader");
 var addImportSpecifiers = require("steal-config-utils/import-specifiers").addImportSpecifiers;
 
 function template(imports, intermediate, filename){
+	var tagImportNames = JSON.stringify(imports.slice(5));
 	imports = JSON.stringify(imports);
 	intermediate = JSON.stringify(intermediate);
 
 	return "define("+imports+",function(module, assign, stache, mustacheCore, viewImport, bindings){ \n" +
-		"\tstache.addBindings(bindings);\n"+
+    "\tstache.addBindings(bindings);\n"+
 		(filename ?
 			"\tvar renderer = stache(" + JSON.stringify(filename) + ", " + intermediate + ");\n" :
 			"\tvar renderer = stache(" + intermediate + ");\n"
 		) +
+    "\tvar tagImports = arguments.slice(6);\n"
 		"\treturn function(scope, options, nodeList){\n" +
 		"\t\tvar moduleOptions = assign({}, options);\n" +
+    "\t\tvar tagImportMap = " + tagImportNames + ".reduce((map, name, index) => {\n" +
+    "\t\t\tmap[name] = tagImports[index];\n" +
+    "\t\t\treturn map;\n" +
+    "\t\t}, {});\n" +
+    "\n"+
 		"\t\tif(moduleOptions.helpers) {\n" +
-		"\t\t\tmoduleOptions.helpers = assign({ module: module }, moduleOptions.helpers);\n" +
+		"\t\t\tmoduleOptions.helpers = assign({ module: module, tagImportMap: tagImportMap }, moduleOptions.helpers);\n" +
 		"\t\t} else {\n" +
 		"\t\t\tmoduleOptions.module = module;\n" +
+		"\t\t\tmoduleOptions.tagImportMap = tagImportMap;\n" +
 		"\t\t}\n" +
+		"\n" +
 		"\t\treturn renderer(scope, moduleOptions, nodeList);\n" +
 		"\t};\n" +
 	"});";
